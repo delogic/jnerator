50cec9865b60c4bebc64084b2533ca0e9668974d
Celio Silva <celio@delogic.com.br>
2014-06-04 23:07:09 -0300

    Fix random date generation

--- src/test/java/br/com/delogic/jnerator/util/RandomUtilTest.java
+++ src/test/java/br/com/delogic/jnerator/util/RandomUtilTest.java
@@ -9,35 +9,33 @@ public class RandomUtilTest extends Assert {
 
     private double yearPeriod = 365.25 * 24 * 60 * 60 * 1000;
 
+    private Date dayFrom(){
+        return new Date((long) (System.currentTimeMillis() - yearPeriod));
+    }
+
+    private Date dayTo(){
+        return new Date((long) (System.currentTimeMillis() + yearPeriod));
+    }
+
     @Test
     public void testGetDateBetween() {
-        Date dayFrom = new Date((long) (System.currentTimeMillis() - yearPeriod));
-        Date dayTo = new Date((long) (System.currentTimeMillis() + yearPeriod));
-
         for (int i = 0; i < 10000000; i++) {
             Date randomDate = RandomUtil.getDateBetween(2);
-            assertTrue(randomDate.compareTo(dayFrom) > 0);
-            assertTrue(randomDate.compareTo(dayTo) < 0);
+            assertTrue(randomDate.compareTo(dayFrom()) > 0);
+            assertTrue(randomDate.compareTo(dayTo()) < 0);
         }
 
     }
 
     @Test
     public void testGetDateTo() {
-        Date dayFrom = new Date((long) (System.currentTimeMillis() - yearPeriod));
-        Date dayTo = new Date((long) (System.currentTimeMillis() + yearPeriod));
-
         for (int i = 0; i < 10000000; i++) {
             Date futureRandomDate = RandomUtil.getDateTo(1);
             assertTrue(futureRandomDate.compareTo(new Date()) > 0);
-            if (!(futureRandomDate.compareTo(dayTo) < 0 )){
-                System.out.println("Future date:" + futureRandomDate);
-                System.out.println("DayTo:" + dayTo);
-            }
-            assertTrue(futureRandomDate.compareTo(dayTo) <= 0);
+            assertTrue(futureRandomDate.compareTo(dayTo()) <= 0);
 
             Date pastRandomDate = RandomUtil.getDateTo(-1);
-            assertTrue(pastRandomDate.compareTo(dayFrom) > 0);
+            assertTrue(pastRandomDate.compareTo(dayFrom()) > 0);
             assertTrue(pastRandomDate.compareTo(new Date()) < 0);
         }
 
