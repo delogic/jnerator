cb8df8e9ff76e29849450b12743635fe0a2f9110
Celio Silva <celio@delogic.com.br>
2014-06-14 14:32:45 -0300

    Included String attribute generation from derived values

--- src/main/java/br/com/delogic/jnerator/impl/generator/StringAttributeGenerator.java
+++ src/main/java/br/com/delogic/jnerator/impl/generator/StringAttributeGenerator.java
@@ -2,10 +2,28 @@ package br.com.delogic.jnerator.impl.generator;
 
 import br.com.delogic.jnerator.AttributeConfiguration;
 import br.com.delogic.jnerator.AttributeGenerator;
+import br.com.delogic.jnerator.util.Util;
 
 public class StringAttributeGenerator implements AttributeGenerator<String> {
 
+    private String[][] values;
+
+    public StringAttributeGenerator(String[]... values) {
+        this.values = values;
+    }
+
+    public StringAttributeGenerator() {}
+
     public String generate(int index, AttributeConfiguration attributeConfiguration, Object instance) {
+        if (values != null) {
+            StringBuilder sb = new StringBuilder();
+            for (String[] arr : values) {
+                sb.append(arr[Util.nextInt(arr.length)]);
+                sb.append(" ");
+            }
+            return sb.toString().trim();
+        }
+
         return attributeConfiguration.getName() + " " + index;
     }
 
--- src/main/java/br/com/delogic/jnerator/util/Util.java
+++ src/main/java/br/com/delogic/jnerator/util/Util.java
@@ -1,11 +1,17 @@
 package br.com.delogic.jnerator.util;
 
+import java.util.Random;
+
 public class Util {
 
+    private static final Random random = new Random();
+
     public static final int validIndex(int index, int size) {
-
         return index % size;
+    }
 
+    public static final int nextInt(int limit){
+        return random.nextInt(limit);
     }
 
 }
